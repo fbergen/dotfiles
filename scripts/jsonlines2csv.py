0ffaad8d48a467f4ed4682c5f3c5d3e8c8f42319
Fredrik Bergenlid <fredrik.bergenlid@gmail.com>
2018-07-23 18:13:19 +0200

    support nested fields

--- scripts/jsonlines2csv.py
+++ scripts/jsonlines2csv.py
@@ -3,12 +3,26 @@
 import sys
 import json
 import csv
-import io
 import argparse
+import copy
 
 def read_records(f):
     for line in f:
         yield json.loads(line)
+
+def flatten(records):
+    for record in records:
+        flat = copy.deepcopy(record)
+        flatten_recursive(record, flat, parent="")
+        yield flat
+
+def flatten_recursive(record, out, parent=""):
+    for key, value in record.items():
+        new_parent = parent + "." + key if parent else key
+        if isinstance(value, dict):
+            flatten_recursive(value, out, parent=new_parent)
+        else:
+            out[new_parent] = value
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Convert jsonlines file to csv.')
@@ -21,6 +35,6 @@ if __name__ == "__main__":
     args = parser.parse_args()
     writer = csv.DictWriter(args.out, fieldnames=args.fields.split(','), extrasaction='ignore')
     writer.writeheader()
-    for row in read_records(vars(args)['in']):
+    for row in flatten(read_records(vars(args)['in'])):
         writer.writerow(row)
 
