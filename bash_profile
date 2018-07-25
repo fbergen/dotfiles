4a1c9b60a08fb1ae33e2f144a9a0fda797a76cac
Fredrik Bergenlid <fredrik.bergenlid@gmail.com>
2018-07-25 15:19:55 +0200

    add coiption to not flattening

--- bash_profile
+++ bash_profile
@@ -32,3 +32,8 @@ export ANDROID_HOME=${HOME}/Library/Android/sdk
 export PATH=${PATH}:${ANDROID_HOME}/tools
 export PATH=${PATH}:${ANDROID_HOME}/platform-tools
 
+
+# Setting PATH for Python 3.6
+# The original version is saved in .bash_profile.pysave
+PATH="/Library/Frameworks/Python.framework/Versions/3.6/bin:${PATH}"
+export PATH
--- scripts/jsonlines2csv.py
+++ scripts/jsonlines2csv.py
@@ -11,8 +11,16 @@ def read_records(f):
         yield json.loads(line)
 
 def flatten(records):
+    """
+    Flatten the record and creating top level "." separated keys for nested records.
+    This will still keep any nested records so that we can still lookup by first level keys
+    e.g.
+    in: {foo: {bar: baz}}
+    out: {foo.bar: {baz}, foo: {bar: baz}}
+    """
     for record in records:
-        flat = copy.deepcopy(record)
+        # Copy the record to make sure we keep the old structure too.
+        flat = record
         flatten_recursive(record, flat, parent="")
         yield flat
 
@@ -27,7 +35,11 @@ def flatten_recursive(record, out, parent=""):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Convert jsonlines file to csv.')
     parser.add_argument('--fields', metavar='field', required=True,
-                        help='A comma separated list of fieldnames in the json object to output to csv')
+                        help='A comma separated list of fieldnames in the json object to output to csv. ' +
+                             'If flattening is enabled, also support dot-separated field names to lookup nested keys')
+    parser.add_argument('--noflat', dest='noflat', action='store_const',
+                        const=True, default=False,
+                        help='Disable record flattening, can improve performance')
     parser.add_argument('in', nargs='?', metavar='jsonl_in', type=argparse.FileType('r'), default=sys.stdin,
                         help='Infile to parse, should be in json lines format, defaults to stdin')
     parser.add_argument('out', nargs='?', metavar='csv_out', type=argparse.FileType('w'), default=sys.stdout,
@@ -35,6 +47,10 @@ if __name__ == "__main__":
     args = parser.parse_args()
     writer = csv.DictWriter(args.out, fieldnames=args.fields.split(','), extrasaction='ignore')
     writer.writeheader()
-    for row in flatten(read_records(vars(args)['in'])):
-        writer.writerow(row)
 
+    records = read_records(vars(args)['in'])
+    if not args.noflat:
+        # We should flatten
+        records = flatten(records)
+    for row in records:
+        writer.writerow(row)
