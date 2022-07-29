58148ef696e0ba7c423fa5d3af4e0381d1027c6a
Fredrik Bergenlid <fredrik.bergenlid@gmail.com>
2022-07-29 16:04:33 +0200

    install oh-my-zsh and update karabiner config

--- install.sh
+++ install.sh
@@ -5,3 +5,5 @@ sudo pip3 install --upgrade pynvim
 # Install mypy
 python3 -m pip install -U mypy
 
+# Install oh-my-zsh
+ZSH= sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
